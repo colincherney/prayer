96487b3de2c50def4e0755b5ecb35a1748ccc932
cURRYcURRY123 <steve123726@gmail.com>
2026-05-02 11:42:27 -0400

    added icons for the tab bar

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { View } from 'react-native';
 
 import AppTabs from '@/components/app-tabs';
+import { TabIconOverlay } from '@/components/TabIconOverlay';
 
 export default function TabsLayout() {
-  return <AppTabs />;
+  return (
+    <View style={{ flex: 1 }}>
+      <AppTabs />
+      <TabIconOverlay />
+    </View>
+  );
 }
